b75b0e5f402038147a7f6eac96a3a52117421d32
andygeiss <4ndygeiss@gmail.com>
2021-05-04 16:37:05 +0200

    removed

--- clients/cli/main.go
+++ clients/cli/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "log"
+import (
+	"embed"
+	"log"
+
+	"github.com/andygeiss/method2go/services/project/engines"
+)
 
 var (
 	name    string = "no-name"
@@ -8,6 +13,12 @@ var (
 	version string = "no-version"
 )
 
+//go:embed configs
+var content embed.FS
+
 func main() {
 	log.Printf("%s %s (%s)\n", name, version, build)
+
+	te := engines.NewDefaultTemplateEngine(&content, "configs/method2go_template", []string{"main.go"})
+
 }
