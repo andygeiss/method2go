84147d7a39ef7f00a8e6234801432116fb3f93e0
andygeiss <4ndygeiss@gmail.com>
2021-06-04 12:11:41 +0200

    tests fixed

--- services/project/repositories/fileSystem_test.go
+++ services/project/repositories/fileSystem_test.go
@@ -6,20 +6,20 @@ import (
 	"testing"
 
 	"github.com/andygeiss/method2go/services/project"
-	"github.com/andygeiss/method2go/services/project/resources"
+	"github.com/andygeiss/method2go/services/project/repositories"
 	assert "github.com/andygeiss/utilities/testing"
 )
 
 func TestFileSystem_GenerateProjectStructure_Should_Return_Without_An_Error(t *testing.T) {
 	os.RemoveAll("testdata")
-	resourceAccess := resources.NewFileSystem("testdata", []string{"foo/bar/foo.txt"})
+	repository := repositories.NewFileSystem("testdata", []string{"foo/bar/foo.txt"})
 	project := &project.Project{
 		Contents: map[string]string{
 			"foo/bar/foo.txt": "bar",
 		},
 	}
-	err := resourceAccess.GenerateProjectStructure(project)
-	exists := resources.HasFile(filepath.Join("testdata", "foo", "bar", "foo.txt"))
+	err := repository.GenerateProjectStructure(project)
+	exists := repositories.HasFile(filepath.Join("testdata", "foo", "bar", "foo.txt"))
 	assert.That("error should be nil", t, err, nil)
 	assert.That("project should not be nil", t, project == nil, false)
 	assert.That("foo/bar/main.go should exists", t, exists, true)
