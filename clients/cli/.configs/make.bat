f1b8ec1ca58328c5ba397ec11e1d069907738df5
ANDREAS GEIß <Andreas.Geiss@fiduciagad.de>
2021-06-17 15:58:07 +0200

    logging and tracing added to build

--- clients/cli/.configs/make.bat
+++ clients/cli/.configs/make.bat
@@ -18,6 +18,8 @@ echo VERSION = %VERSION%
 echo.
 
 echo start building ...
+go get github.com/andygeiss/utilities/logging
+go get github.com/andygeiss/utilities/tracing
 go build --ldflags "-s -w -X=main.build=%BUILD% -X=main.name=%NAME% -X=main.version=%VERSION%" -o %GOPATH%\bin\%NAME%_api.exe clients\api\main.go
 go build --ldflags "-s -w -X=main.build=%BUILD% -X=main.name=%NAME% -X=main.version=%VERSION%" -o %GOPATH%\bin\%NAME%_cli.exe clients\cli\main.go
 go build --ldflags "-s -w -X=main.build=%BUILD% -X=main.name=%NAME% -X=main.version=%VERSION%" -o %GOPATH%\bin\%NAME%_web.exe clients\web\main.go
